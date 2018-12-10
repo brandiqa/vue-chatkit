3b6b5a49aec734d7fcbbd5e16be8eb66c601da29
Michael Wanyoike <micwanyoike@gmail.com>
2018-12-10 19:19:58 +0300

    Scaffolded Chat

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,5 +1,37 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="chat-dashboard">
+   <b-row>
+     <b-col>
+       <RoomList />
+      </b-col>
+     <b-col>
+       <b-row>
+         <MessageList />
+       </b-row>
+       <b-row>
+         <MessageForm />
+       </b-row>
+      </b-col>
+     <b-col>
+       <UserList />
+      </b-col>
+   </b-row>
   </div>
 </template>
+
+<script>
+import RoomList from '@/components/RoomList.vue'
+import MessageList from '@/components/MessageList.vue'
+import MessageForm from '@/components/MessageForm.vue'
+import UserList from '@/components/UserList.vue'
+
+export default {
+  name: 'Chat',
+  components: {
+    RoomList,
+    UserList,
+    MessageList,
+    MessageForm
+  }
+}
+</script>
