cc18bd3111809b14f344596ca5d5923ff1ec70fa
Michael Wanyoike <micwanyoike@gmail.com>
2018-12-10 19:33:04 +0300

    Installed vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -10,5 +11,6 @@ Vue.use(BootstrapVue)
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
