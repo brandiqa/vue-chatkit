61590ea5c0ad231c317fe57718ddb6f6c7c7350e
Michael Wanyoike <micwanyoike@gmail.com>
2018-12-12 14:37:40 +0300

    Added logout button

--- src/components/ChatNavBar.vue
+++ src/components/ChatNavBar.vue
@@ -3,6 +3,10 @@
     <b-navbar-brand href="#">
       Vue Chat
     </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-text>Display Name</b-nav-text>
+      <b-nav-item href="#" active>Logout</b-nav-item>
+    </b-navbar-nav>
   </b-navbar>
 </template>
 
